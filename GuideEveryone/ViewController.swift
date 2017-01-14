6888d7cc6761af7f0694a62e6a2c7bdaf28eb273
a <a@cat.local>
2017-01-14 16:33:41 +0200

    Initial Commit

---
+++ GuideEveryone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GuideEveryone
+//
+//  Created by a on 14.01.17.
+//  Copyright © 2017 a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
